299038de8385b02358de4f0484224b764e01f7a1
samebere93 <anyanwuse@gmail.com>
2025-02-04 21:03:16 -0800

    refixing task 2

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+""" square module"""
 
-class square():
-    
+
+class Square():
+    """ A square class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ the init for the square class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ the perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ the str rep"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
